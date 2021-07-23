57419b20f89bad0840c28b809ac2c89882e31956
Atai Ambus <atayambus+org@gmail.com>
2021-07-23 14:21:29 +0300

    feat: added script to import data from open source json
    - Body in points
    
    - I found a recipe dataset at https://eightportions.com/datasets/Recipes/ 
    - The dataset contains a json with a list of "recipe" objects, including ingredients and instructions but also title, description, calories, and so on. 
    - get_data.py is a script that converts that JSON (presuming it's donwloaded at dataset.json at the project directory,) to a TCV file with a column for paragraphs and a column for their label (0 for ingredients, 1 for instructions)
    
    - The script opens the file, loads it as a dictionary via the json library, then iterates iver its object and extracts a lst for ingredients and a list for instructions, and trims them to be equal size. In tits final stage, it alternates between these lists and iteratively writes them into a file called dataset.tcv, with an appropriate label for each delimited by a tab.

---
+++ get_data.py
@@ -0,0 +1,49 @@
+import json
+import numpy as np
+
+# Prior, Download dataset json from https://eightportions.com/datasets/Recipes/ to dataset.json
+
+JSON_PATH = 'dataset.json'
+DATA_SIZE = 67_000
+
+
+def get_raw_json():
+    with open(JSON_PATH) as datafile:
+        return datafile.read()
+
+
+def get_json_dict():
+    return json.loads(get_raw_json())
+
+
+def get_data_lists():
+    recipes = get_json_dict()
+    instructions = []
+    ingredients = []
+    for item in recipes:
+        if item:  # JSON contains a few empty recipes for some reason. this check the item is not empty.
+            instructions += item['directions']
+            ingredients += item['ingredients']
+
+    # Total size of instructions is 69458
+    # Total size of ingredients is 199030
+    # I don't really want to try and deal with imbalanced classes in training, so I'll trim them to 67000
+    # I could have really done it while getting them from the recipe dictionaries, but in this case it was pretty
+    # negligible in time
+    return instructions[:DATA_SIZE], ingredients[:DATA_SIZE]
+
+
+def save_to_tsv(instructions, ingredients):
+    instructions, ingredients = get_data_lists()
+    dataset = {}
+    for i in range(DATA_SIZE):
+        # Ignore tabs and newlines to not interrupt the tsv format (the tokenizer does not care about them anyway)
+        dataset[ingredients[i].replace('\t', ' ').replace('\n', '')] = 0
+        dataset[instructions[i].replace('\t', ' ').replace('\n', '')] = 1
+    with open('dataset.tsv', 'w+') as f:
+        for item in dataset.keys():
+            f.write(f"{item}\t{dataset[item]}\n")
+
+
+if __name__ == '__main__':
+    save_to_tsv(*get_data_lists())
