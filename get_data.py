7bb4546c9ab4ea536022f631bf7afa9ce10f47a4
Atai Ambus <atayambus+org@gmail.com>
2021-07-27 07:42:27 +0300

    feat(data): get_data gathers from text files as well
    FUNCTION:
    get_data.py assembles the text files from sort-data.sh into the TSV file together with the JSON. neither data is asssembled partially in the start and partially in the end, for both training and testing data
    
    METHOD:
    - in get_data_lists() which gathered the data from the JSON, read the relevant files and add them to the relevant list. Also add a list for irrelevants in the return statement.
    
    - in save_to_tsv(), which writes the data to the TSV file, I also iterated over neither data. 20000 of it (currently 25000) went in the start and the rest in the end. This is because the final data is cut in the middle for training and testing data.
    
    NOTES:
    I can't iterate over the neither dtaa together with instructions and ingredients, since it's much smaller. Because of this, I can't alternate with them elegantly when writing to the file so I went with the append/prepend solution, though it's somewhat inelegant as well.

--- get_data.py
+++ get_data.py
@@ -4,7 +4,7 @@ import numpy as np
 # Prior, Download dataset json from https://eightportions.com/datasets/Recipes/ to dataset.json
 
 JSON_PATH = 'dataset.json'
-DATA_SIZE = 55_000
+DATA_SIZE = 40_000
 
 
 def get_raw_json():
@@ -17,29 +17,57 @@ def get_json_dict():
 
 
 def get_data_lists():
-    recipes = get_json_dict()
     instructions = []
     ingredients = []
+    neither = []
+    with open('instructions.txt', 'r') as datafile:
+        instructions += datafile.readlines()
+    with open('ingredients.txt', 'r') as datafile:
+        ingredients += datafile.readlines()
+    with open('neither.txt', 'r') as datafile:
+        neither += datafile.readlines()
+    return instructions, ingredients, neither
+
+
+def get_data_lists() -> Tuple[List[str], List[str], List[str]]:
+    instructions = []
+    ingredients = []
+    irrelevants = []
+
+    with open('instructions.txt', 'r') as datafile:
+        instructions += datafile.readlines()
+    with open('ingredients.txt', 'r') as datafile:
+        ingredients += datafile.readlines()
+    with open('neither.txt', 'r') as datafile:
+        irrelevants += datafile.readlines()
+
+    recipes = get_json_dict()
     for item in recipes:
         if item:  # JSON contains a few empty recipes for some reason. this check the item is not empty.
             instructions += item['directions']
             ingredients += item['ingredients']
-    # Total size of instructions is 69458
-    # Total size of ingredients is 199030
-    # I don't really want to try and deal with imbalanced classes in training, so I'll trim them to 67000
-    # I could have really done it while getting them from the recipe dictionaries, but in this case it was pretty
-    # negligible in time
 
-    return list(set(instructions))[:DATA_SIZE], list(set(ingredients))[:DATA_SIZE]
+    return list(set(instructions))[:DATA_SIZE], list(set(ingredients))[:DATA_SIZE], irrelevants
 
 
-def save_to_tsv(instructions, ingredients):
-    instructions, ingredients = get_data_lists()
-    dataset = {}
+def save_to_tsv(instructions, ingredients, irrelevants):
+    dataset = []
+
+    # Ignore tabs and newlines to not interrupt the tsv format (the tokenizer does not care about them anyway)
+    clean = lambda lst: [str(item).replace('\t', ' ').replace('\n', ' ') for item in lst]
+    ingredients = clean(ingredients)
+    instructions = clean(instructions)
+    irrelevants = clean(irrelevants)
+
     for i in range(DATA_SIZE):
-        # Ignore tabs and newlines to not interrupt the tsv format (the tokenizer does not care about them anyway)
-        dataset[ingredients[i].replace('\t', ' ').replace('\n', '')] = 0
-        dataset[instructions[i].replace('\t', ' ').replace('\n', '')] = 1
+        dataset.append([ingredients[i], '1,0,0'])
+        dataset.append([instructions[i], '0,1,0'])
+
+    for item in irrelevants[:20_000]:
+        dataset.insert(0, [item, '0,0,1'])
+    for item in irrelevants[20_000:]:
+        dataset.append([item, '0,0,1'])
+
     with open('dataset.tsv', 'w+') as f:
         for item in dataset.keys():
             f.write(f"{item}\t{dataset[item]}\n")
