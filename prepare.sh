170c8bfae51147fc5448ad9e7668f9be2aca961d
Atai Ambus <atayambus+org@gmail.com>
2021-07-29 20:15:41 +0300

    feat(run): added bash script to run data and model generation
    - Added a script that regenerates the TSV and TXT data files from the JSON and scraping URLs, and then retrains the  and recreates the tokenizers models and saves them
    
    - Basically, it runs `./scrape_data.py && ./assemble_data.py && ./classifier.py && ./trashbot.py` , redirects stderr to a tempfile and if there is an error informs the user.
    
    - This allows both to easily update the models based on new data and to fix lost/corrupted data.

---
+++ prepare.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+err_file=$(mktemp)
+python3 -c "from scrape_data import reset; reset()" 2>"$err_file"  \
+&& python3 ./assemble_data.py 2>"$err_file" \
+&& python3 ./classifier.py 2>"$err_file" \
+&& python3 ./trashbot.py 2>"$err_file"
+
+# Suppress warning to use command in if block directly rather than checking status later. I think it's more mess
+# than it's worth.
+# shellcheck disable=SC2181
+if [ "$?" -ne 0 ]; then
+  echo "An error occurred. Check $err_file for details."
+  cat "$err_file"
+fi
